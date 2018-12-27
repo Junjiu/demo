61aa47ef9832747aa4df0756fb50781aa1a40d35
Junjie Wen <junjie@iastate.edu>
2018-12-28 06:06:47 +0800

    Save

--- db/mongoUtil.js
+++ db/mongoUtil.js
@@ -9,15 +9,32 @@ var url = "mongodb://localhost:27017/";
         return target;
     }
 module.exports ={
-	insertItem: (itemInfor ) => {
+	insertItem: (itemInfor, callback ) => {
 		MongoClient.connect(url, { useNewUrlParser: true }, function(err, db) {
  			if (err) throw err;
 			console.log(itemInfor);
   			var dbo = db.db("items");
-			dbo.collection("itemsInfor").insertOne(itemInfor, function(err, res) {
+			var itemInforJSON = {};
+/*			
+			if(itemInfor['title'] && itemInfor['price'] && itemInfor['description'] && itemInfor['location'] && itemInfor['location']['type'] && itemInfor['location']['coordinates']){
+			}else{
+				callback("illegal input");
+				db.close();
+				return; 
+			}
+*/	
+			itemInforJSON['title'] = itemInfor['title'];
+			itemInforJSON['price'] = itemInfor['price'];
+			itemInforJSON['description'] = itemInfor['description'];
+			itemInforJSON['location'] = {};
+			itemInforJSON['location']['type'] = itemInfor['location']['type'];
+			itemInforJSON['location']['coordinates'] = itemInfor['location']['coordinates'];			
+			itemInforJSON['openid'] = itemInfor['openid'];
+			itemInforJSON['image'] = itemInfor['image'];
+			dbo.collection("itemsInfor").insertOne(itemInforJSON, function(err, res) {
     			if (err) throw err;
-    			console.log("1 item  inserted");
-    			db.close();
+    			callback("one item has be inserted to db");
+			db.close();
   			});
 		});
 	},
@@ -36,7 +53,6 @@ module.exports ={
                  MongoClient.connect(url, { useNewUrlParser: true }, function(err, db) {
                         if (err) throw err;
                         var dbo = db.db("items");
-			console.log(id);
                         dbo.collection("itemsInfor").find({'_id':new ObjectId(id)} ).toArray(function(e, d){
                                 db.close();
                                 console.log(d);
@@ -71,5 +87,69 @@ module.exports ={
 			});
 			
 		});
-	}		
+	},
+	insertUser : (userInfor, openid, callback) =>{
+		   MongoClient.connect(url, { useNewUrlParser: true }, function(err, db) {
+                        if (err) throw err;
+                        var dbo = db.db("users");
+                	var userInforJSON = {};	 
+                	userInforJSON['openid'] = openid;
+			userInforJSON['avatarUrl'] = userInfor.avatarUrl;
+			userInforJSON['city'] = userInfor.city;
+			userInforJSON['country'] = userInfor.country;
+			userInforJSON['gender'] = userInfor.gender;
+			userInforJSON['language'] = userInfor.language;
+			userInforJSON['nickName'] = userInfor.nickName;
+			userInforJSON['province'] = userInfor.province;
+			dbo.collection("userInfor").insertOne(userInforJSON, function(err, res) {
+                        	if (err) throw err;
+                        	callback("one user has be inserted to db");
+                        	db.close();
+                        });
+		});		
+	},
+	getUser : (openid, callback) =>{
+		 MongoClient.connect(url, { useNewUrlParser: true }, function(err, db) {
+                        if (err) throw err;
+                        var dbo = db.db("users");
+                        dbo.collection("userInfor").find({'openid':openid} ).toArray(function(e, d){
+                                db.close();
+                                callback(d);
+                        });
+                });	
+	},
+	setFavorite : (openid, itemId, callback) =>{
+		MongoClient.connect(url, { useNewUrlParser: true }, function(err, db) {
+                        if (err) throw err;
+			var favoriteJSON = {};
+			favoriteJSON['openid'] = openid;
+			favoriteJSON['itemId'] = itemId;
+                        var dbo = db.db("items");
+                        dbo.collection("favorite").insertOne(favoriteJSON, function(err, res) {
+                                if (err) throw err;
+                                callback("added a favorte");
+                                db.close();
+                        });
+                });
+	},
+	 getFavorite : (openid, callback) =>{
+                 MongoClient.connect(url, { useNewUrlParser: true }, function(err, db) {
+                        if (err) throw err;
+                        var dbo = db.db("items");
+                        dbo.collection("favorite").find({'openid':openid} ).toArray(function(e, d){
+                                db.close();
+                                callback(d);
+                        });
+                });
+        },
+	getItemsByUser : (openid, callback) =>{
+                 MongoClient.connect(url, { useNewUrlParser: true }, function(err, db) {
+                        if (err) throw err;
+                        var dbo = db.db("items");
+                        dbo.collection("itemsInfor").find({'openid':openid} ).toArray(function(e, d){
+                                db.close();
+                                callback(d);
+                        });
+                });
+        }
 }
--- routes/index.js
+++ routes/index.js
@@ -1,14 +1,17 @@
 var express = require('express');
 var router = express.Router();
+var request = require('request');
 var formidable = require('formidable');
     util = require('util');
 var multiparty = require('multiparty');
 var fs = require('fs');
 var db = require('./../db/mongoUtil');
 router.post('/update',  function(req, res, next){
-	db.insertItem(req.body);	
-	res.send("one item has be inserted to db");
+	console.log("update!");
+	db.insertItem(req.body, (message) =>{
+	res.send(message);
 	res.end();
+	});	
 });
 
 router.post('/updateImage', function(req, res){
@@ -43,8 +46,37 @@ router.get('/item', function(req, res){
 });
 router.get('/getNearItems', function(req, res){
 	db.getNearItems(function(items){
-		console.log(items);
 		res.send(items);
 	}, req.query.latitude, req.query.longitude, req.query.page);
+});
+router.post('/login', function(req, res){
+	var url = 'https://api.weixin.qq.com/sns/jscode2session?appid=wx85ee5650ad5bcb23&secret=ed270ff5dce1d388e94fc603e24b612f&js_code='+ req.body.code + '&grant_type=authorization_code';
+	request(url, function (error, response, body) {
+		var resJSON = JSON.parse(response.body);
+		db.insertUser(req.body.userInfo,resJSON.openid, (result) => {
+			console.log(result);
+		});
+		res.send(response.body);
+	});	
+});
+router.get('/getUser', function(req, res){
+	db.getUser(req.query.openid, (result) =>{
+		res.send(result);
+	});
+});
+router.get('/getFavorite', function(req, res){
+	db.getFavorite(req.query.openid, (result) => {
+		res.send(result);
+	});
+});
+router.get('/setFavorite', function(req,res){
+	db.setFavorite(req.query.openid, req.query.itemId, (result) =>{
+		res.send(result);
+	});
+});
+router.get('/getItemsByUser', function(req, res){
+	db.getItemsByUser(req.query.openid, (result)=>{
+		res.send(result);
+	});
 });
 module.exports = router;
